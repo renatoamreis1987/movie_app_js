5aba2bae6abe63826a60dc25442957d38ec01540
Renato Reis <email@renatoamreis.com>
2021-03-20 13:09:30 +0100

    Missing comparing results

--- index.js
+++ index.js
@@ -1,33 +1,99 @@
-const fetchData = async (searchTerm) => {
+// AutoComplete Config Standard for any possibility !!
+
+const autoCompleteConfig = {
+  renderOption(movie) {
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    return `
+        <img src="${imgSrc}" />
+        ${movie.Title} (${movie.Year})
+      `;
+  },
+
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "3ec8bebd",
+        s: searchTerm,
+      },
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+    return response.data.Search;
+  },
+};
+
+// To create 2 AutoCompletes without duplicate code
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector("#left-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden')
+    onMovieSelect(movie, document.querySelector('#left-summary'));
+  },
+});
+
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector("#right-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden')
+    onMovieSelect(movie, document.querySelector('#right-summary'));
+  },
+});
+
+// Get Movie Initial Info
+const onMovieSelect = async (movie, summaryElement) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "3ec8bebd",
-      s: searchTerm,
+      i: movie.imdbID,
     },
   });
 
-  if (response.data.Error) {
-    return [];
-  }
-
-  return response.data.Search;
+  summaryElement.innerHTML = movieTemplate(response.data);
 };
 
-const input = document.querySelector("input");
-
-const onInput = async (event) => {
-  const movies = await fetchData(event.target.value);
-
-  for (let movie of movies) {
-    const div = document.createElement("div");
-
-    div.innerHTML = `
-      <img src="${movie.Poster}" />
-      <h1>${movie.Title}</h1>
-    `;
-
-    document.querySelector("#target").appendChild(div);
-  }
+// Movie Template
+const movieTemplate = (movieDetail) => {
+  return `
+  <article class="media">
+    <figure class="media-left">
+      <p class="image">
+        <img src="${movieDetail.Poster}" />
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="contente">
+        <h1>${movieDetail.Title}</h1>
+        <h4>${movieDetail.Genre}</h4>
+        <p>${movieDetail.Plot}</p>
+      </div>
+    </div>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
+  </article>
+  `;
 };
-
-input.addEventListener("input", debounce(onInput, 500));
